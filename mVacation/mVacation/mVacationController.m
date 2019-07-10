1e9629b35f44cfd04d5fa04254a853d126a7bd68
Navjot Singh <51707035+NaitraNaitra@users.noreply.github.com>
2019-07-10 12:29:53 +0100

    Add files via upload

--- mVacation/mVacation/mVacationController.m
+++ mVacation/mVacation/mVacationController.m
@@ -10,16 +10,26 @@
 #import "Destination.h"
 
 @implementation mVacationController
+@synthesize transactionField;
+@synthesize balanceField;
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    NSString* europeText = [[NSString alloc] initWithFormat: @"%@", @"Europe" ];
+    europe = [[Destination alloc] initWithCountry:europeText andBudget:10000.00 withExchangeRate:1.25];
+    [balanceField setStringValue:[[NSString alloc] initWithFormat:@"%.2f", [europe leftToSpend]]];
+}
 
 - (IBAction)spendDollars:(id)sender {
-    NSLog(@"Sending a %.2f cash transaction",[_transactionField.stringValue floatValue]);
-    [europe spendCash:[_transactionField.stringValue doubleValue]];
-    [_balanceField setStringValue:[[NSString alloc]initWithFormat:@"%.2f",[europe leftToSpend]]];
+    NSLog( @"Sending a %.2f cash transaction", [transactionField.stringValue floatValue] );
+    [europe spendCash:[transactionField.stringValue doubleValue]];
+    [balanceField setStringValue:[[NSString alloc] initWithFormat:@"%.2f", [europe leftToSpend]]];
 }
 
 - (IBAction)chargeCreditCard:(id)sender {
-    NSLog(@"Sending a %.2f credit card transaction",[_transactionField.stringValue floatValue]);
-    [europe chargeCreditCard:[_transactionField.stringValue floatValue]];
-    [_balanceField setStringValue:[[NSString alloc]initWithFormat:@"%.2f",[europe leftToSpend]]];
+    NSLog( @"Sending a %.2f credit card transaction", [transactionField.stringValue floatValue] );
+    [europe chargeCreditCard:[transactionField.stringValue doubleValue]];
+    [balanceField setStringValue:[[NSString alloc] initWithFormat:@"%.2f", [europe leftToSpend]]];
 }
 @end
